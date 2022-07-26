9eb06656837a5ae65fcac217bb83ff6bdb26d5ab
dwydm <dwydmuch@gmail.com>
2022-07-26 21:46:52 +0200

    SortingAlgorithm

--- src/main/java/SDAKurs/Dzien9/SortingAlgorithm.java
+++ src/main/java/SDAKurs/Dzien9/SortingAlgorithm.java
@@ -6,10 +6,27 @@ public class SortingAlgorithm {
 
 
     public ArrayList<Integer> selectionSort(ArrayList<Integer> list) {
-        ArrayList<Integer> sortedList = new ArrayList<>();
+        int min;
+        int index;
 
+        for (int i = 0; i < list.size(); i++) {
+            min = list.get(i);
+            index = i;
+            for (int j = i; j < list.size(); j++) {
+                if(min > list.get(j)) {
+                    min = list.get(j);
+                    index = j;
+                }
+            }
+            if (index == i) {
+                continue;
+            }
+            list.set(index, list.get(i));
+            list.set(i,min);
 
-        return sortedList;
+        }
+
+        return list;
     }
 
 }
