6e6b8cf9a30155c1497b0302a3e9ff525284c73b
dwydm <dwydmuch@gmail.com>
2022-08-07 20:48:39 +0200

    Task 31

--- src/main/java/SDATasks/Zad31/PetDemo.java
+++ src/main/java/SDATasks/Zad31/PetDemo.java
@@ -5,41 +5,56 @@ import java.time.Month;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.lang.System.out;
 
 public class PetDemo {
     public static void main(String[] args) {
 
         //Task setup
-        Person john = new Person("John", LocalDate.of(1990, Month.JANUARY,01));
-        Person anna = new Person("Anna", LocalDate.of(1997,Month.APRIL,04));
-        Person kamila = new Person("Kamila", LocalDate.of(2000,Month.JUNE,02));
+        Person john = new Person("John", LocalDate.of(1990, Month.JANUARY, 01));
+        Person anna = new Person("Anna", LocalDate.of(1997, Month.APRIL, 04));
+        Person kamila = new Person("Kamila", LocalDate.of(2000, Month.JUNE, 02));
 
-        john.getPets().add(new Pet("Tom",6));
+        john.getPets().add(new Pet("Tom", 6));
         john.getPets().add(new Pet("Jerry", 3));
         john.getPets().add(new Pet("Butcher", 6));
 
-        anna.getPets().add(new Pet("Ara",2));
-        anna.getPets().add(new Pet("Cookie",4));
+        anna.getPets().add(new Pet("Ara", 2));
+        anna.getPets().add(new Pet("Cookie", 4));
 
-        List<Person> people = new ArrayList<>(Arrays.asList(john,kamila,anna));
+        List<Person> people = new ArrayList<>(Arrays.asList(john, kamila, anna));
 
 
         //a
         people.stream().map((person) -> person.getName())
                 .sorted(Comparator.naturalOrder())
                 .toList()
-                .forEach(name -> System.out.println(name));
+                .forEach(name -> out.println(name));
 
         //b
         people.stream().map(person -> person.getBirthDay().getMonth().getValue())
                 .toList()
-                .forEach(month -> System.out.println(month));
+                .forEach(out::println);
 
         //c
-        System.out.println(people.stream().flatMap((person) -> person.getPets().stream()).count());
+        people.stream().flatMap(person -> Stream.of(person.getPets().size())).forEach(out::println);
 
         //d
+        out.println("Total animal age: " + people.stream()
+                .map(Person::getPets)
+                .flatMap(petList -> petList.stream())
+                .collect(Collectors.toList())
+                .stream()
+                .mapToInt(Pet::getAge)
+                .sum());
 
-
+        //f
+        out.println("Average owner age: " +people.stream()
+                .mapToInt(Person::getAge)
+                .sum() / people.stream()
+                .count());
     }
 }
